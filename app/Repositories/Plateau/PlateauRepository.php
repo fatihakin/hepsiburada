112d919e01ccef76c15c17e6c0626e5d0791b739
Fatih AKIN <fatihakin75@gmail.com>
2022-06-03 21:53:43 +0300

    Repository Service Pattern was added

--- app/Repositories/Plateau/PlateauRepository.php
+++ app/Repositories/Plateau/PlateauRepository.php
@@ -5,4 +5,13 @@ namespace App\Repositories\Plateau;
 class PlateauRepository implements PlateauRepositoryInterface
 {
 
+    public function getAllPlateaus(array $orderDetails)
+    {
+        // TODO: Implement getAllPlateaus() method.
+    }
+
+    public function createPlateau(array $orderDetails)
+    {
+        // TODO: Implement createPlateau() method.
+    }
 }
